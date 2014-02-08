46424e230d984c7509d4bb8328d8e913697ebbf3
Wilson Yeh <wilsonye@usc.edu>
2014-02-08 14:48:34 -0800

    ???

--- FallDetectionApp/src/com/usc/ss12/falldetectionapp/MainActivity.java
+++ FallDetectionApp/src/com/usc/ss12/falldetectionapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.Menu;
 import android.view.KeyEvent;
 import java.util.Timer;
 import java.util.TimerTask;
